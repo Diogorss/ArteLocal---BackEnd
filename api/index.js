58aba6fd85253254af1118571d89607b191525d8
DIogo Rossi Sampaio <108912348+Diogorss@users.noreply.github.com>
2025-06-06 16:23:10 +0000

    removendo debug

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../.env') }); // Ajuste: só um nível acima (../) a partir de api/index.js
-console.log("DEBUG: Todas as variáveis carregadas:", process.env);
+
 
 
 
